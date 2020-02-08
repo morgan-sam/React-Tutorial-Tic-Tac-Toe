e21df7fa45b32e9164b1d95d82f3722ce12b30b5
Sam <samuel_morgan_@outlook.com>
2020-02-08 23:14:52 +0000

    ability to select board dimension from menu

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const GAME_TILE_DIMENSION = 5;
 const LINE_WIN_SIZE = 3;
 
 function Square(props) {
@@ -28,10 +27,10 @@ class Board extends React.Component {
 
 	render() {
 		const items = [];
-		for (let i = 0; i < GAME_TILE_DIMENSION; i++) {
+		for (let i = 0; i < this.props.boardDimension; i++) {
 			let children = [];
-			for (let j = 0; j < GAME_TILE_DIMENSION; j++) {
-				children.push(this.renderSquare(j + i * GAME_TILE_DIMENSION));
+			for (let j = 0; j < this.props.boardDimension; j++) {
+				children.push(this.renderSquare(j + i * this.props.boardDimension));
 			}
 			items.push(
 				<div className="board-row" key={i}>
@@ -45,56 +44,13 @@ class Board extends React.Component {
 	}
 }
 
-class Menu extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			gameActive: true
-		};
-	}
-
-	render() {
-		let boardDimChildren = [];
-		for (let i = 3; i <= 10; i++) {
-			boardDimChildren.push(
-				<option value={i} key={i}>
-					{i}
-				</option>
-			);
-		}
-
-		return (
-			<div>
-				<div style={{ display: this.state.gameActive ? 'block' : 'none' }}>
-					<button onClick={() => this.toggleGame(!this.state.gameActive)}>Start</button>
-					<select defaultValue={'DEFAULT'}>
-						<option value="DEFAULT" disabled hidden>
-							Select Board Dimensions
-						</option>
-						{boardDimChildren}
-					</select>
-				</div>
-				<div style={{ display: !this.state.gameActive ? 'block' : 'none' }}>
-					<Game />
-				</div>
-			</div>
-		);
-	}
-
-	toggleGame(boo) {
-		this.setState({
-			gameActive: boo
-		});
-	}
-}
-
 class Game extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			history: [
 				{
-					squares: Array(Math.pow(GAME_TILE_DIMENSION, 2)).fill(null)
+					squares: Array(Math.pow(this.props.boardDimension, 2)).fill(null)
 				}
 			],
 			xIsNext: true,
@@ -103,11 +59,10 @@ class Game extends React.Component {
 	}
 
 	handleClick(i) {
-		console.log(this.state);
 		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
-		if (calculateWonTiles(squares).length > 0 || squares[i]) {
+		if (this.calculateWonTiles(squares).length > 0 || squares[i]) {
 			return;
 		}
 		squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -129,10 +84,71 @@ class Game extends React.Component {
 		});
 	}
 
+	calculateWonTiles(squares) {
+		const lines = this.calculateWinningLines(this.props.boardDimension, LINE_WIN_SIZE);
+		let coords = Array(this.props.boardDimension).fill(null);
+		for (let i = 0; i < lines.length; i++) {
+			coords = lines[i].map((el) => squares[el]);
+			if (this.allEqual(coords)) return lines[i];
+		}
+		return [];
+	}
+
+	calculateWinningLines(dimension, winsize) {
+		let winningLines = [];
+
+		//horizontal
+		let horizontal = [];
+		for (let i = 0; i < dimension; i++) {
+			horizontal.push([ ...Array(dimension).keys() ].map((x) => x + i * dimension));
+		}
+		//vertical
+		let vertical = [];
+		for (let i = 0; i < dimension; i++) {
+			vertical.push([ ...Array(dimension).keys() ].map((x) => dimension * x + i));
+		}
+
+		//diagonals
+		let diagonal = [];
+
+		for (let i = -dimension + winsize; i <= dimension - winsize; i++) {
+			//downright, starts top centre tile, finishes starting on left centre
+			diagonal.push(
+				[ ...Array(dimension - Math.abs(i)).keys() ].map((x) => -i + (dimension + 1) * (x + Math.max(0, i)))
+			);
+			//downleft, starts right centre tile, finishes starting on top centre
+			diagonal.push(
+				[ ...Array(dimension - Math.abs(i)).keys() ].map(
+					(x) => dimension * i + (dimension - 1) * (1 + x - Math.min(0, i))
+				)
+			);
+		}
+
+		winningLines.push(this.splitLinesIntoWinLines(horizontal));
+		winningLines.push(this.splitLinesIntoWinLines(vertical));
+		winningLines.push(this.splitLinesIntoWinLines(diagonal));
+		winningLines = winningLines.flat();
+		return winningLines;
+	}
+
+	splitLinesIntoWinLines(input, winsize = LINE_WIN_SIZE) {
+		let winlines = [];
+		input.forEach(function(el) {
+			for (let i = 0; i < el.length - (winsize - 1); i++) {
+				winlines.push(el.slice(i, i + winsize));
+			}
+		});
+		return winlines;
+	}
+
+	allEqual(arr) {
+		return arr.every((el) => el === arr[0] && arr[0] !== null);
+	}
+
 	render() {
 		const history = this.state.history;
 		const current = history[this.state.stepNumber];
-		let winningSquares = calculateWonTiles(current.squares);
+		let winningSquares = this.calculateWonTiles(current.squares);
 
 		const moves = history.map((step, move) => {
 			const desc = move ? 'Go to move #' + move : 'Go to game start';
@@ -154,6 +170,7 @@ class Game extends React.Component {
 			<div className="game">
 				<div className="game-board">
 					<Board
+						boardDimension={this.props.boardDimension}
 						winningSquares={winningSquares}
 						squares={current.squares}
 						onClick={(i) => this.handleClick(i)}
@@ -167,67 +184,61 @@ class Game extends React.Component {
 		);
 	}
 }
-
-function calculateWonTiles(squares) {
-	const lines = calculateWinningLines(GAME_TILE_DIMENSION, LINE_WIN_SIZE);
-	let coords = Array(GAME_TILE_DIMENSION).fill(null);
-	for (let i = 0; i < lines.length; i++) {
-		coords = lines[i].map((el) => squares[el]);
-		if (allEqual(coords)) return lines[i];
-	}
-	return [];
-}
-
-function calculateWinningLines(dimension, winsize) {
-	let winningLines = [];
-
-	//horizontal
-	let horizontal = [];
-	for (let i = 0; i < dimension; i++) {
-		horizontal.push([ ...Array(dimension).keys() ].map((x) => x + i * dimension));
-	}
-	//vertical
-	let vertical = [];
-	for (let i = 0; i < dimension; i++) {
-		vertical.push([ ...Array(dimension).keys() ].map((x) => dimension * x + i));
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			gameActive: false,
+			boardDimensions: 3
+		};
 	}
 
-	//diagonals
-	let diagonal = [];
-
-	for (let i = -dimension + winsize; i <= dimension - winsize; i++) {
-		//downright, starts top centre tile, finishes starting on left centre
-		diagonal.push(
-			[ ...Array(dimension - Math.abs(i)).keys() ].map((x) => -i + (dimension + 1) * (x + Math.max(0, i)))
-		);
-		//downleft, starts right centre tile, finishes starting on top centre
-		diagonal.push(
-			[ ...Array(dimension - Math.abs(i)).keys() ].map(
-				(x) => dimension * i + (dimension - 1) * (1 + x - Math.min(0, i))
-			)
-		);
+	toggleGame(boo) {
+		this.setState({
+			gameActive: boo
+		});
+	}
+	setBoardDimensions(value) {
+		this.setState({
+			boardDimensions: value
+		});
 	}
 
-	winningLines.push(splitLinesIntoWinLines(horizontal));
-	winningLines.push(splitLinesIntoWinLines(vertical));
-	winningLines.push(splitLinesIntoWinLines(diagonal));
-	winningLines = winningLines.flat();
-	return winningLines;
-}
-
-function splitLinesIntoWinLines(input, winsize = LINE_WIN_SIZE) {
-	let winlines = [];
-	input.forEach(function(el) {
-		for (let i = 0; i < el.length - (winsize - 1); i++) {
-			winlines.push(el.slice(i, i + winsize));
+	render() {
+		let boardDimChildren = [];
+		for (let i = 3; i <= 10; i++) {
+			boardDimChildren.push(
+				<option value={i} key={i}>
+					{i}
+				</option>
+			);
 		}
-	});
-	return winlines;
+		let gameDisplay, menuOptions;
+		if (this.state.gameActive) {
+			gameDisplay = [ <Game boardDimension={this.state.boardDimensions} key={'game'} /> ];
+			menuOptions = null;
+		} else {
+			gameDisplay = null;
+			menuOptions = [
+				<button onClick={() => this.toggleGame(true)}>Start</button>,
+				<select defaultValue={'DEFAULT'} onChange={(e) => this.setBoardDimensions(parseInt(e.target.value))}>
+					<option value="DEFAULT" disabled hidden>
+						Select Board Dimensions
+					</option>
+					{boardDimChildren}
+				</select>
+			];
+		}
+
+		return (
+			<div>
+				{menuOptions}
+				{gameDisplay}
+			</div>
+		);
+	}
 }
 
-function allEqual(arr) {
-	return arr.every((el) => el === arr[0] && arr[0] !== null);
-}
 // ========================================
 
-ReactDOM.render(<Menu />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
